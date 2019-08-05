09288e47e8bfee4584e995a6c01fa782deecd2c6
John DeFalco <jdefalco@gmail.com>
2019-08-04 21:51:52 -0400

    Added some output of the standaings to the index.cshtml razor page

--- Presentation/Razor/Pages/Index.cshtml.cs
+++ Presentation/Razor/Pages/Index.cshtml.cs
@@ -1,17 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using RunsPoolTracker.AppService;
+using RunsPoolTracker.Model;
 
 namespace Razor.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        public TeamRunsCollection TeamRunsCollection { get; set; }
 
+        public async Task OnGet()
+        {
+            Initialize(out DateTime roundStartDate, out MlbAppService appService);
+            TeamRunsCollection = await appService.ProcessDailyGamesForRound(roundStartDate);
+        }
+
+        private static void Initialize(out DateTime roundStartDate, out MlbAppService appService)
+        {
+            IConfigurationRoot configuration = InitializeConfiguration();
+            var roundStartDateFromConfig = $"round4_start_date";
+            roundStartDate = Convert.ToDateTime(configuration[roundStartDateFromConfig]);
+            appService = new MlbAppService();
+        }
+
+        private static IConfigurationRoot InitializeConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+            return configuration;
         }
     }
 }
