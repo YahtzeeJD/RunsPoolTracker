4af3f260ca752d0c6d9e8c08861f95bd7daebb4f
John DeFalco <jdefalco@gmail.com>
2019-08-11 14:30:28 -0400

    Created a true home page. Added the publish profile to deploy to SmarterASP.NET

--- Presentation/Razor/Pages/Index.cshtml.cs
+++ Presentation/Razor/Pages/Index.cshtml.cs
@@ -1,43 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
-using RunsPoolTracker.AppService;
-using RunsPoolTracker.Model;
 
 namespace Razor.Pages
 {
     public class IndexModel : PageModel
     {
-        public TeamRunsCollection TeamRunsCollection { get; set; }
-        public int Round { get; set; }
-
         public async Task OnGet()
         {
-            Initialize(out DateTime roundStartDate, out MlbAppService appService);
-            TeamRunsCollection = await appService.ProcessDailyGamesForRound(roundStartDate);
-        }
-
-        private void Initialize(out DateTime roundStartDate, out MlbAppService appService)
-        {
-            IConfigurationRoot configuration = InitializeConfiguration();
-            Round = 4;
-            var roundStartDateFromConfig = $"round{Round}_start_date";
-            roundStartDate = Convert.ToDateTime(configuration[roundStartDateFromConfig]);
-            appService = new MlbAppService();
-        }
-
-        private IConfigurationRoot InitializeConfiguration()
-        {
-            var builder = new ConfigurationBuilder()
-                                .SetBasePath(Directory.GetCurrentDirectory())
-                                .AddJsonFile("appsettings.json");
-            var configuration = builder.Build();
-            return configuration;
         }
     }
 }
---
+++ Presentation/Razor/Pages/Standings.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using RunsPoolTracker.AppService;
+using RunsPoolTracker.Model;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Razor.Pages
+{
+    public class StandingsModel : PageModel
+    {
+        public TeamRunsCollection TeamRunsCollection { get; set; }
+        public int Round { get; set; }
+
+        public async Task OnGet()
+        {
+            Initialize(out DateTime roundStartDate, out MlbAppService appService);
+            TeamRunsCollection = await appService.ProcessDailyGamesForRound(roundStartDate);
+        }
+
+        private void Initialize(out DateTime roundStartDate, out MlbAppService appService)
+        {
+            IConfigurationRoot configuration = InitializeConfiguration();
+            Round = 4;
+            var roundStartDateFromConfig = $"round{Round}_start_date";
+            roundStartDate = Convert.ToDateTime(configuration[roundStartDateFromConfig]);
+            appService = new MlbAppService();
+        }
+
+        private IConfigurationRoot InitializeConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+            return configuration;
+        }
+    }
+}
